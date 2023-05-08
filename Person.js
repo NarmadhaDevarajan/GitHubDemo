859a8a9610ca5ebab4c9c59d76653bdb6c079b11
NarmadhaDevarajan <127471806+NarmadhaDevarajan@users.noreply.github.com>
2023-05-08 20:32:17 +0530

    Add files via upload

---
+++ Person.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(name,age,gender)
+    {
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+}
+let person1=new Person("Narmadha",33,"Female");
+let person2=new Person("Vijay",35,"male");
+console.log(person1,person2);
